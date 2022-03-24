5d9a8971039659f10f56d6d57054ddd42eaa9223
Patrick Brennan <brennanpatrick20@gmail.com>
2022-03-24 19:56:23 -0400

    Add slime character to menu

--- game/menu.py
+++ game/menu.py
@@ -19,6 +19,11 @@ class Menu(pygame.Surface):
         info_1 = pygame.transform.scale(self.font.render("https://github.com/AM2i9", 8, (255, 255, 255)), (320, 32))
         info_2 = pygame.transform.scale(self.font.render("Created for Pyweek 33", 8, (255, 255, 255)), (320, 32))
 
+        slime_thing = pygame.transform.scale(
+            pygame.image.load("assets/slime_monster_spritesheet.png").convert_alpha(),
+            (1536, 1536),
+        ).subsurface((0, 512*2, 512, 512))
+
         while True:
 
             self.blit(title, title.get_rect().move(32, 32))
@@ -28,10 +33,15 @@ class Menu(pygame.Surface):
             self.blit(info_2, info_2.get_rect().move(32, 610))
             self.blit(info_1, info_1.get_rect().move(32, 650))
 
+            self.blit(slime_thing, slime_thing.get_rect().move(640, 240))
+
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_RETURN:
+                        return
 
             win.blit(self, self.get_rect())
 
